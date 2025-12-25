177ede53a8591329645001f86e2a4c6ec5a37bb2
mousamax <mousa.abdelmaksoud@tum.de>
2025-12-25 22:39:45 +0100

    Add repository interfaces for Family, Member, TaskList, and Task entities

---
+++ src/main/java/com/familymind/powersync/repository/FamilyRepository.java
@@ -0,0 +1,11 @@
+package com.familymind.powersync.repository;
+
+import com.familymind.powersync.entity.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface FamilyRepository extends JpaRepository<Family, UUID> {
+}
---
+++ src/main/java/com/familymind/powersync/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.familymind.powersync.repository;
+
+import com.familymind.powersync.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
