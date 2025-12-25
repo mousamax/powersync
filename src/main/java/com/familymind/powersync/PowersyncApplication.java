a3a0a73293cecec59b8fca8d4a0288ef311a746d
mousamax <mousa.abdelmaksoud@tum.de>
2025-12-25 15:40:23 +0100

    Spring init

---
+++ src/main/java/com/familymind/powersync/PowersyncApplication.java
@@ -0,0 +1,13 @@
+package com.familymind.powersync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowersyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowersyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/familymind/powersync/PowersyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familymind.powersync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowersyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
